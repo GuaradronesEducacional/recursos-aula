655c390a94cec81d7030074d75c22e927c642208
guaradrones <54602036+guaradrones@users.noreply.github.com>
2019-09-18 15:50:58 -0300

    Add files via upload

---
+++ Foguete print.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foguete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ControleTeclado();
+    }
+    private void ControleTeclado()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Comando utilizado para identificar uma tecla pressionada
+        {
+            print("acelerando");  // Ação Realizada pelo comando
+        }
+        if (Input.GetKey(KeyCode.A)) // Comando utilizado para identificar uma tecla pressionada
+        {
+            print("giro esquerda"); // Ação Realizada pelo comando
+        }
+        else if (Input.GetKey(KeyCode.D)) // Comando utilizado para identificar uma tecla pressionada
+        { 
+            print("giro direita"); // Ação Realizada pelo comando
+        }
+    }
+}
