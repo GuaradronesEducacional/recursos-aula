fe1cbede557d06eb3fa666a1a9f6a8d543111ecd
guaradrones <54602036+guaradrones@users.noreply.github.com>
2019-10-02 14:39:50 -0300

    Add files via upload

---
+++ Level Load.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float Aceleracao = 100f;
+    [SerializeField] float Giro = 100f;
+
+    Rigidbody rigidBody;
+   
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+       }
+	
+	// Update is called once per frame
+	void Update () {
+        Acelerar();
+        Girar();
+	}
+
+	 void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+				print("ok");
+                
+                break;
+            case "Finish":
+                print("Hit finish"); 
+                SceneManager.LoadScene(1);
+                break;
+			
+            default:
+                print("Dead");
+               
+                break;
+        }
+    }
+
+    private void Acelerar()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rigidBody.AddRelativeForce(Vector3.up * Aceleracao);
+           
+        }
+        
+    }
+
+    private void Girar()
+    {
+        rigidBody.freezeRotation = true;
+       
+        float rotationThisFrame = Giro * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; 
+    }
+}
